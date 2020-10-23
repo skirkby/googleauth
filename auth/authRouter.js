69807e28f8210a1217b5ed19ebf116dd4bb26f7c
Sean Kirkby <skirkby@kirkby.org>
2020-10-23 14:41:15 -0600

    access people on the return object

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -29,7 +29,7 @@ router.get('/register', async (req, res, next) => {
         const url = 'https://people.googleapis.com/v1/people/me?personFields=names';
         url = 'https://people.googleapis.com/v1/people:listDirectoryPeople';
         const apiData = await oAuth2Client.request({ url });
-        console.log(apiData.data);
+        console.log(apiData.people);
 
 
         res.json({ message: 'api up', tokens: oAuth2Client.credentials, data: apiData });
