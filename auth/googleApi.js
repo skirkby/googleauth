c93c45df32a4e0d32f33f59adbfc8bdff4367a32
Sean Kirkby <skirkby@kirkby.org>
2020-10-24 00:28:04 -0600

    fix oauth keys access

--- auth/googleApi.js
+++ auth/googleApi.js
@@ -6,16 +6,21 @@ let oAuth2Client = undefined;
 
 function config() {
 
+    console.log("dirname: ", __dirname);
     // load oauth2 keys from keypath in environment, 
     // or default to ./oauth2.keys.json
     const keyPath = process.env.OAUTH_KEYS_PATH ?
         process.env.OAUTH_KEYS_PATH :
-        path.join(__dirname, 'oauth2.keys.json');
+        path.join(__dirname, '..\\oauth2.keys.json');
+
+    console.log(keyPath);
 
     let keys = { redirect_uris: [''] };
     if (fs.existsSync(keyPath)) {
         keys = require(keyPath).web;
     }
+
+    console.log(keys);
 
     oAuth2Client = new google.auth.OAuth2(
         keys.client_id,
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('./auth/googleApi.js').config();
 
 const server = require('./api/server.js');
 
